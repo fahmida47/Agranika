6073376a89b96ddaf0a83c55dbde749f013f0821
afija0022-655 <afija0022@gmail.com>
2026-03-14 05:35:22 -0500

    update homepage

--- agranika/src/App.js
+++ agranika/src/App.js
@@ -27,24 +27,43 @@ function App() {
           </div>
         </div>
 
-        <div
-          className="hero-section"
-          style={{ backgroundImage: `url(${heroImg})` }}
-        >
+        <div className="hero-section">
+          <div
+            className="hero-bg"
+            style={{ backgroundImage: `url(${heroImg})` }}
+          ></div>
+
           <div className="hero-overlay"></div>
 
-          <div className="plus-sign">+</div>
-          <div className="slide-number">4</div>
+          
 
           <div className="headline-box">
             <h1>
               Leading Children
               <br />
               Toward
-              <br />a Brighter
+              <br />
+              a Brighter
               <br />
               Future
             </h1>
+          </div>
+
+          <div className="right-gallery">
+            <div
+              className="gallery-card card-small"
+              style={{ backgroundImage: `url(${heroImg})` }}
+            ></div>
+
+            <div
+              className="gallery-card card-tall"
+              style={{ backgroundImage: `url(${heroImg}) `}}
+            ></div>
+
+            <div
+              className="gallery-card card-medium"
+              style={{ backgroundImage: `url(${heroImg})` }}
+            ></div>
           </div>
         </div>
       </div>
