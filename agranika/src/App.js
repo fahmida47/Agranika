7de5d2305e4a942d1c84149304c676c1795d8f1d
afija0022-655 <afija0022@gmail.com>
2026-03-14 04:41:04 -0500

    homepage added

---
+++ agranika/src/App.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "./App.css";
+
+import logo from "./assets1/Logo2.png";
+import heroImg from "./assets1/Home2.jpg";
+
+function App() {
+  return (
+    <div className="page">
+      <div className="main-box">
+        <div className="navbar">
+          <div className="logo-box">
+            <img src={logo} alt="Logo" className="logo" />
+          </div>
+
+          <div className="nav-menu">
+            <a href="/">Home</a>
+            <a href="/">Mission</a>
+            <a href="/">Focus</a>
+            <a href="/">About Us</a>
+            <a href="/">Donate</a>
+          </div>
+
+          <div className="nav-right">
+            <span className="search-icon">&#128269;</span>
+            <button className="sponsor-btn">Sponsor a Child</button>
+          </div>
+        </div>
+
+        <div
+          className="hero-section"
+          style={{ backgroundImage: `url(${heroImg})` }}
+        >
+          <div className="hero-overlay"></div>
+
+          <div className="plus-sign">+</div>
+          <div className="slide-number">4</div>
+
+          <div className="headline-box">
+            <h1>
+              Leading Children
+              <br />
+              Toward
+              <br />a Brighter
+              <br />
+              Future
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
