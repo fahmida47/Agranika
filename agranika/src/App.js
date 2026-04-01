057bad14e046429d8a7a09ccb956f81ee68f154a
Fahmida Afrin Nadia <fahmida.200547@gmail.com>
2026-04-02 00:36:57 +0600

    Update App.js

--- agranika/src/App.js
+++ agranika/src/App.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react";
-import Intro from "./Components/Intro";
-import Login from "./Components/Login";
-import Signup from "./Components/Signup";
-import ForgotPassword from "./Components/ForgotPassword";
+import Navbar from "./components/Navbar";
+import Intro from "./components/Intro";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import ForgotPassword from "./components/ForgotPassword";
+import HomePage from "./components/HomePage";
+import Focus from "./components/Focus";
+import Mission from "./components/Mission";
+import DonatePage from "./components/DonatePage"; 
 
 function App() {
   const [page, setPage] = useState("intro");
@@ -11,30 +16,61 @@ function App() {
   useEffect(() => {
     const timer1 = setTimeout(() => setFadeOut(true), 2500);
     const timer2 = setTimeout(() => setPage("login"), 3500);
-
     return () => {
       clearTimeout(timer1);
       clearTimeout(timer2);
     };
   }, []);
 
+  // Pages where Navbar should be visible
+  const showNavbar = ["home", "focus", "mission", "donate"].includes(page);
+
   return (
     <div>
-      {page === "intro" && <Intro fadeOut={fadeOut} />}
+      {/* Show Navbar only on these pages */}
+      {showNavbar && (
+        <Navbar
+          goHome={() => setPage("home")}
+          goMission={() => setPage("mission")}
+          goFocus={() => setPage("focus")}
+          goDonate={() => setPage("donate")}
+          goLogin={() => setPage("login")}
+        />
+      )}
 
+      {page === "intro" && <Intro fadeOut={fadeOut} />}
       {page === "login" && (
         <Login
           toggleSignup={() => setPage("signup")}
           goForgot={() => setPage("forgot")}
+          goHome={() => setPage("home")}
         />
       )}
-
-      {page === "signup" && (
-        <Signup toggleLogin={() => setPage("login")} />
-      )}
-
+      {page === "signup" && <Signup toggleLogin={() => setPage("login")} />}
       {page === "forgot" && (
         <ForgotPassword toggleLogin={() => setPage("login")} />
+      )}
+      {page === "home" && (
+        <HomePage 
+          goMission={() => setPage("mission")}
+          goFocus={() => setPage("focus")}
+          goDonate={() => setPage("donate")}
+        />
+      )}
+      {page === "focus" && (
+        <Focus
+          goHome={() => setPage("home")}
+          goMission={() => setPage("mission")}
+        />
+      )}
+      {page === "mission" && (
+        <Mission
+          goHome={() => setPage("home")}
+          goLogin={() => setPage("login")}
+        />
+      )}
+      {page === "donate" && (
+        <DonatePage goHome={() => setPage("home")} />
       )}
     </div>
   );
